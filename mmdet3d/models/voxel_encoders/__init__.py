b9a9354443d823577076189f1ebbbf18a6b91942
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-28 01:04:05 -0700

    Update __init__.py

--- mmdet3d/models/voxel_encoders/__init__.py
+++ mmdet3d/models/voxel_encoders/__init__.py
@@ -2,10 +2,10 @@
 from .pillar_encoder import DynamicPillarFeatureNet, PillarFeatureNet
 from .voxel_encoder import (DynamicSimpleVFE, DynamicVFE, HardSimpleVFE,
                             HardVFE, SegVFE)
-from .fsd_vfe import FSDVFE
+from .fsd_vfe import FSDVFE, FSDv2VFE
 
 
 __all__ = [
     'PillarFeatureNet', 'DynamicPillarFeatureNet', 'HardVFE', 'DynamicVFE',
-    'HardSimpleVFE', 'DynamicSimpleVFE', 'SegVFE', 'FSDVFE'
+    'HardSimpleVFE', 'DynamicSimpleVFE', 'SegVFE', 'FSDVFE', 'FSDv2VFE'
 ]
