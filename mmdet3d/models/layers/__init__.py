9d73c10e6fef23b32fcf22d8d921028646423241
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-28 00:27:45 -0700

    Update __init__.py

--- mmdet3d/models/layers/__init__.py
+++ mmdet3d/models/layers/__init__.py
@@ -20,6 +20,7 @@ from .torchsparse_block import (TorchSparseBasicBlock, TorchSparseBottleneck,
                                 TorchSparseConvModule)
 from .transformer import GroupFree3DMHA
 from .vote_module import VoteModule
+from .focal_sparse_conv import FocalSparseConv, MemoryEfficientFocalSparseConv
 
 __all__ = [
     'VoteModule', 'GroupFree3DMHA', 'EdgeFusionModule', 'DGCNNFAModule',
@@ -32,5 +33,5 @@ __all__ = [
     'PointFPModule', 'PAConvSAModule', 'PAConvSAModuleMSG',
     'PAConvCUDASAModule', 'PAConvCUDASAModuleMSG', 'TorchSparseConvModule',
     'TorchSparseBasicBlock', 'TorchSparseBottleneck', 'MinkowskiConvModule',
-    'MinkowskiBasicBlock', 'MinkowskiBottleneck'
+    'MinkowskiBasicBlock', 'MinkowskiBottleneck', 'FocalSparseConv', 'MemoryEfficientFocalSparseConv'
 ]
