e28a582d84f2f377db099328ac5c59cf1f37a8f2
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-28 00:31:53 -0700

    Update __init__.py

--- mmdet3d/models/necks/__init__.py
+++ mmdet3d/models/necks/__init__.py
@@ -6,8 +6,10 @@ from .imvoxel_neck import IndoorImVoxelNeck, OutdoorImVoxelNeck
 from .pointnet2_fp_neck import PointNetFPNeck
 from .second_fpn import SECONDFPN
 from .squeeze_fpn import SQUEEZEFPN
+from .fsd_neck import FSDNeck
+
 
 __all__ = [
     'FPN', 'SECONDFPN', 'OutdoorImVoxelNeck', 'PointNetFPNeck', 'DLANeck',
-    'IndoorImVoxelNeck','SQUEEZEFPN'
+    'IndoorImVoxelNeck','SQUEEZEFPN', 'FSDNeck'
 ]
