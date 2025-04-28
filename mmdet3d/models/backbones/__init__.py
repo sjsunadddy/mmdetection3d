822dfc65b6b1a84adb07f80457f5c6f798f8d7b0
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-28 00:54:40 -0700

    Update __init__.py

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -20,5 +20,6 @@ __all__ = [
     'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'NoStemRegNet',
     'SECOND', 'DGCNNBackbone', 'PointNet2SASSG', 'PointNet2SAMSG',
     'MultiBackbone', 'DLANet', 'MinkResNet', 'Asymm3DSpconv',
-    'MinkUNetBackbone', 'SPVCNNBackbone', 'MinkUNetBackboneV2','SQUEEZE'
+    'MinkUNetBackbone', 'SPVCNNBackbone', 'MinkUNetBackboneV2','SQUEEZE',
+    'FSD', 'FSDv2'
 ]
