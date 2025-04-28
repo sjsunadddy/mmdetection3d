617179eb9ab44a8a0f5a795ecbb919413eb12ee0
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-28 00:30:28 -0700

    Update __init__.py

--- mmdet3d/models/middle_encoders/__init__.py
+++ mmdet3d/models/middle_encoders/__init__.py
@@ -3,10 +3,10 @@ from .pillar_scatter import PointPillarsScatter
 from .sparse_encoder import SparseEncoder, SparseEncoderSASSD
 from .sparse_unet import SparseUNet
 from .voxel_set_abstraction import VoxelSetAbstraction
-from .fsd_neck import FSDNeck
+from .fsd_middle_encoder import FSDMiddleEncoder
 
 
 __all__ = [
     'PointPillarsScatter', 'SparseEncoder', 'SparseEncoderSASSD', 'SparseUNet',
-    'VoxelSetAbstraction', 'FSDNeck'
+    'VoxelSetAbstraction', 'FSDMiddleEncoder'
 ]
