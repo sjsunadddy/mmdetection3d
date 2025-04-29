d049eed843ab83c6fe495fc53c1079e999c22a4e
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-04-29 00:16:27 -0700

    Update mvxnet_sqeezefpn_voxelNext_kitti-3d-3class.py

--- configs/mvxnet/mvxnet_sqeezefpn_voxelNext_kitti-3d-3class.py
+++ configs/mvxnet/mvxnet_sqeezefpn_voxelNext_kitti-3d-3class.py
@@ -255,7 +255,7 @@ val_evaluator = dict(
 test_evaluator = val_evaluator
 
 # training schedule
-train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=5, val_interval=1)
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=20, val_interval=4)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
 
