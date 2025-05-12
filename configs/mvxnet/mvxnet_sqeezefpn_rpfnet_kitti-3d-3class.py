84ba1a3b42acdfc300f0d0513b63d2cfdbe4cd70
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-05-11 23:48:54 -0700

    Update mvxnet_sqeezefpn_rpfnet_kitti-3d-3class.py

--- configs/mvxnet/mvxnet_sqeezefpn_rpfnet_kitti-3d-3class.py
+++ configs/mvxnet/mvxnet_sqeezefpn_rpfnet_kitti-3d-3class.py
@@ -71,7 +71,7 @@ model = dict(
         sparse_shape=[41, 1600, 1408],
         order=('conv', 'norm', 'act')),
 
-    # ----------------------- PillarNet-LTS backbone -------------
+    # ----------------------- RPFNet backbone -------------
     pts_backbone=dict(
         type='RPFNet',
         in_channels=256,               # output of SparseEncoder
