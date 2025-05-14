ac7e65141c1f1bb8294a5ccfbeb737ef5dccc0be
Aravind Singh Bisht <166579628+aravindbisht@users.noreply.github.com>
2025-05-14 14:03:36 -0700

    fix: adding new FireRPFNet backbone

--- mmdet3d/models/backbones/__init__.py
+++ mmdet3d/models/backbones/__init__.py
@@ -4,6 +4,7 @@ from mmdet.models.backbones import SSDVGG, HRNet, ResNet, ResNetV1d, ResNeXt
 from .cylinder3d import Asymm3DSpconv
 from .dgcnn import DGCNNBackbone
 from .dla import DLANet
+from .fire_rpfnet import FireRPFNet
 from .mink_resnet import MinkResNet
 from .minkunet_backbone import MinkUNetBackbone
 from .multi_backbone import MultiBackbone
@@ -18,5 +19,6 @@ __all__ = [
     'ResNet', 'ResNetV1d', 'ResNeXt', 'SSDVGG', 'HRNet', 'NoStemRegNet',
     'SECOND', 'DGCNNBackbone', 'PointNet2SASSG', 'PointNet2SAMSG',
     'MultiBackbone', 'DLANet', 'MinkResNet', 'Asymm3DSpconv',
-    'MinkUNetBackbone', 'SPVCNNBackbone', 'MinkUNetBackboneV2','SQUEEZE'
+    'MinkUNetBackbone', 'SPVCNNBackbone', 'MinkUNetBackboneV2','SQUEEZE',
+    'FireRPFNet'
 ]
